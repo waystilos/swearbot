f185afcec4a202d3362c1934bfb55d323912ba0b
iLoveNazook <ardonbailey@gmail.com>
2018-03-04 09:45:23 -0800

    first commit with botkit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+require("dotenv").config();
+const bot_token = process.env.SLACK_TOKEN;
+
+const Botkit = require('botkit');
+const controller = Botkit.slackbot();
+const bot = controller.spawn({
+    token: bot_token
+});
+
+const channelId = "C9AL0E7L3";
+
+bot.startRTM((err, bot, payload) => {
+    if(err) {
+        throw new Error('Not working foo');
+    }
+});
+
+controller.on("message_received", (bot, message) => {
+  bot.reply(message, "I heard... something!" + message.teams);
+});
+
+controller.hears(
+  "damn",
+  ["direct_message", "direct_mention", "mention", "ambient"],
+  (bot, message) => {
+      console.dir(message);
+    bot.reply(message, `<@${message.user}> said damn.`);
+  }
+);
+
+
+
